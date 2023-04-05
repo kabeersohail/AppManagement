fed33f9e149b673a0dacd10d51c4e226ca53e545
KabeerSohail <sohail.mohammad@wenable.com>
2023-04-05 12:39:44 +0530

    added flow for v3

--- app/src/test/java/com/weguard/appmanagement/AppManagementV3Test.kt
+++ app/src/test/java/com/weguard/appmanagement/AppManagementV3Test.kt
@@ -12,16 +12,6 @@ import org.junit.Test
 
 class AppManagementV3Test {
 
-    // Create mock objects
-    private val apps = listOf(
-        App("https:://www.example1.com", "111111111", 14354L, "com.example1.android", "Example1"),
-        App("https:://www.example2.com", "222222222", 22434L, "com.example2.android", "Example2"),
-        App("https:://www.example3.com", "333333333", 34355L, "com.example3.android", "Example3"),
-        App("https:://www.example4.com", "444444444", 42343L, "com.example4.android", "Example4"),
-        App("https:://www.example5.com", "555555555", 52343L, "com.example5.android", "Example5"),
-        App("https:://www.example6.com", "666666666", 65445L, "com.example6.android", "Example6")
-    )
-
     @Before
     fun setup() {
         mockkStatic(Log::class)
